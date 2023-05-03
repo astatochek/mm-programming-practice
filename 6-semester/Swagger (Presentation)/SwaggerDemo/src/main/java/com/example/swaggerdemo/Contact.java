56e58deb2dbaee60e78de3311c30cf2d7bafaa8e
astatochek <71458498+astatochek@users.noreply.github.com>
2023-05-03 19:43:34 +0300

    add more Annotation examples

--- 6-semester/Swagger (Presentation)/SwaggerDemo/src/main/java/com/example/swaggerdemo/Contact.java
+++ 6-semester/Swagger (Presentation)/SwaggerDemo/src/main/java/com/example/swaggerdemo/Contact.java
@@ -1,12 +1,20 @@
 package com.example.swaggerdemo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(name = "Contact", description = "A contact in the repository", requiredProperties = {"id", "name"})
 public class Contact {
+
+    @Schema(description = "Contact ID", example = "1")
     private String id;
+
+    @Schema(description = "Contact name", example = "John Doe")
     private String name;
+
+    @Schema(description = "Contact phone", example = "123-456-789")
     private String phone;
 }
--- 6-semester/Swagger (Presentation)/SwaggerDemo/src/main/java/com/example/swaggerdemo/Controller.java
+++ 6-semester/Swagger (Presentation)/SwaggerDemo/src/main/java/com/example/swaggerdemo/Controller.java
@@ -1,10 +1,16 @@
 package com.example.swaggerdemo;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,7 +22,17 @@ import java.util.concurrent.ConcurrentMap;
 public class Controller {
     ConcurrentMap<String, Contact> contacts = new ConcurrentHashMap<>();
 
-    @Operation(summary = "Get a contact by id")
+    @Tags({
+            @Tag(name = "contacts", description = "Operations related to contacts")
+    })
+    @Operation(summary = "Get a contact by id",
+            description = "Returns a single contact",
+            parameters = { @Parameter(in = ParameterIn.PATH,
+                    name = "id",
+                    required = true,
+                    description = "Is the identification of the contact",
+                    schema = @Schema(type = "string",
+                            example = "1")) })
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found the contact",
                     content = { @Content(mediaType = "application/json",
@@ -30,15 +46,39 @@ public class Controller {
         return contacts.get(id);
     }
 
+    @Tags({
+            @Tag(name = "contacts", description = "Operations related to contacts")
+    })
     @GetMapping("/")
     public List<Contact> getAllContacts() {
         return new ArrayList<>(contacts.values());
     }
 
+    @Tags({
+            @Tag(name = "contacts", description = "Operations related to contacts")
+    })
     @PostMapping("/")
     public Contact addContact(@RequestBody Contact contact) {
         contacts.put(contact.getId(), contact);
         return contact;
     }
+
+    @Hidden
+    @GetMapping("/secret")
+    public String secretEndpoint() {
+        // implementation here
+        return "";
+    }
+
+    @Tags({
+            @Tag(name = "not contacts", description = "Operations not related to contacts")
+    })
+    @Deprecated
+    @GetMapping("/old")
+    public String oldEndpoint() {
+        // implementation here
+        return "";
+    }
+
 
 }
